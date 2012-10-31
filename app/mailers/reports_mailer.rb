a2cf5a42e41513f25824642434afef99f48314ce
Rafael Mazza <rafaelmazza@gmail.com>
2012-10-31 14:57:30 -0200

    refactored email reports

--- app/mailers/reports_mailer.rb
+++ app/mailers/reports_mailer.rb
@@ -1,15 +1,21 @@
 class ReportsMailer < ActionMailer::Base
-  default :from => 'do-not-reply@cafeazulhost.com.br'
-  
+  default :from => 'Leads <do-not-reply@cafeazulhost.com.br>'
+
   def daily_leads_total_report(report)
     @report = report
-    subject = 'Relatorio Semanal'
-    mail(:to => 'rafael@cafeazul.com.br', :subject => subject)
+    @subject = 'Leads do dia'
+    mail(to: 'rafael@cafeazul.com.br', subject: @subject, template_name: 'report')
   end
   
   def weekly_leads_total_report(report)
     @report = report
-    subject = 'Relatorio Semanal'
-    mail(:to => 'rafael@cafeazul.com.br', :subject => subject)
+    @subject = 'Leads semanais'
+    mail(to: 'rafael@cafeazul.com.br', subject: @subject, template_name: 'report')
+  end
+  
+  def monthly_leads_total_report(report)
+    @report = report
+    @subject = 'Leads mensais - MultiCommerce'
+    mail(to: 'rafael@cafeazul.com.br', subject: @subject, template_name: 'report')
   end
 end
