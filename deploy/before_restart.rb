f3d568b805c3f1294ff1833faf007501aa529b95
Rafael Mazza <rafaelmazza@gmail.com>
2012-11-05 11:12:09 -0200

    redis restart on deploy hook

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,5 +1,6 @@
 on_app_servers_and_utilities do
   node[:applications].each do |app_name, data|
-    echo "sleep 20 && monit -g #{app}_sidekiq restart all" | at now 
+    run "sudo monit restart all -g redis"
+    run "sudo monit restart all -g sidekiq_multi_commerce_0"
   end
 end
